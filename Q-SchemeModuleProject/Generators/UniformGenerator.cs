0e5c06cf429ebc72982f6fe70434af25654122b9
Pavel_Kasyanov <paulkassianov@gmail.com>
2015-04-26 11:11:04 +0300

    add uniform generator

---
+++ Q-SchemeModuleProject/Generators/UniformGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q_SchemeModuleProject.Generators
+{
+    public class UniformGenerator : Generator
+    {
+        public override double Generate()
+        {
+            double t =  Rnd.NextDouble();
+            return t;
+        }
+    }
+}
